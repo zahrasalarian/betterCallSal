4c26233cb14b0fcc332e3ec7ad6c39c08a624069
amberbrown4 <zasalarian2000@gmail.com>
2020-10-24 16:09:42 +0330

    Updated updateCovid and covidMap arguments

--- R/covidMap.R
+++ R/covidMap.R
@@ -12,13 +12,16 @@
 #'
 
 covidMap <-
-  function()
+  function(date,type)
   {
+    date_temp <- unlist(strsplit(date,'-'))
+    if (as.integer(date_temp[1]) <= 3 & as.integer(date_temp[2]) <= 22)
+      return("date is not valid")
     #get covid data
-    covid<- betterCallSal::updateCovid()
+    covid<- betterCallSal::updateCovid(date)
 
     # set domain
-    domain <- range(covid$Deaths)
+    domain <- range(covid[[type]])
 
     # make palette
     pal <- colorNumeric(palette = viridis(10000), domain = domain)
@@ -27,9 +30,9 @@ covidMap <-
     covidMap <- leaflet(covid)  %>%
       addProviderTiles(providers$CartoDB.DarkMatter) %>%
       setView(-71.931180, 42.385453, zoom = 7) %>%
-      addCircleMarkers(~Long_, ~Lat, popup=covid$Deaths, weight = 3, radius=4,
-                       color=~pal(Deaths), stroke = F, fillOpacity = 0.5)%>%
-      addLegend("bottomright", pal = pal, values = ~Deaths,
-                title = "Deaths")
+      addCircleMarkers(~Long_, ~Lat, popup=covid[[type]], weight = 3, radius=4,
+                       color=~pal(covid[[type]]), stroke = F, fillOpacity = 0.5)%>%
+      addLegend("bottomright", pal = pal, values = ~covid[[type]],
+                title = type)
     return(covidMap)
   }
--- R/updateCovid.R
+++ R/updateCovid.R
@@ -12,13 +12,14 @@
 #'
 
 updateCovid <-
-  function()
+  function(date)
   {
     library(GetoptLong)
     library(readr)
-    date <- unlist(strsplit(as.character(Sys.Date()),'-'))
-    date[3]<- as.character(as.integer(date[3]) - 2)
-    date <- paste(date[2],date[3],date[1],sep="-")
+    #date <- unlist(strsplit(date,'-'))
+    #if (date == as.character(Sys.Date()))
+    #  date[3]<- as.character(as.integer(date[3]) - 1)
+    #date <- paste(date[2],date[3],date[1],sep="-")
     library(RCurl)
     link <- qq("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/@{date}.csv")
     covidData<-as.data.frame(read.csv((link)))
